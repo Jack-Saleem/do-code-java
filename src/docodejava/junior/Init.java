89b9f4ba9f8d57a5954a5ea1e7b22dcf23e430d0
Jack-Saleem <saleem.81311@gmail.com>
2026-02-06 13:20:01 +0530

    Initial java project

---
+++ src/docodejava/junior/Init.java
@@ -0,0 +1,7 @@
+package docodejava.junior;
+
+public class Init {
+    public static void main(String[] args) {
+        System.out.println("From Package docodejava.junior");
+    }
+}
---
+++ src/docodejava/senior/Init.java
@@ -0,0 +1,7 @@
+package docodejava.senior;
+
+public class Init {
+    public static void main(String[] args) {
+        System.out.println("From Package docodejava.senior");
+    }
+}
