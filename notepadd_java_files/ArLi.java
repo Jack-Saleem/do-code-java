00248c33f513ab5cdad776665925cb980b7bf895
sadikshaik <jafarsadikshaik.48@gmail.com>
2026-06-13 20:08:53 +0530

    adding practiced programs

---
+++ notepadd_java_files/ArLi.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class ArLi {
+public static void main(String[] args) {
+ArrayList a = new ArrayList();
+a.add(10);
+a.add("sadik");
+a.add(true);
+System.out.println(a);
+}
+}
---
+++ notepadd_java_files/Sample.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class ArrayList {
+}
+public class Sample{
+public static void main(String[] args) {
+ArrayList a = new ArrayList();
+}
+}
